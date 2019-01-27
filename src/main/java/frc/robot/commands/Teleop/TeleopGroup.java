06d3c6d7c3543183a0155d9d2441a148a7108e31
Aragon Robotics <aragon.robotics.team@gmail.com>
2019-01-27 12:25:05 -0800

    Arcade

--- src/main/java/frc/robot/commands/Teleop/TeleopGroup.java
+++ src/main/java/frc/robot/commands/Teleop/TeleopGroup.java
@@ -5,6 +5,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class TeleopGroup extends CommandGroup {
 
     public TeleopGroup() {
-        addSequential(new ControlDrivetrain());
+        addSequential(new ControlArcadeDrivetrain());
     }
 }
