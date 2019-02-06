7e7e63ec1de34f921c25ce6d0207fdaffddade9d
Aragon Robotics <aragon.robotics.team@gmail.com>
2019-02-05 17:05:15 -0800

    pogress

--- src/main/java/frc/robot/commands/Autonomous/AutoCalibrate.java
+++ src/main/java/frc/robot/commands/Autonomous/AutoCalibrate.java
@@ -14,30 +14,33 @@ public class AutoCalibrate extends Command implements PIDOutput {
     // Uses Ziegler–Nichols method: https://en.wikipedia.org/wiki/Ziegler%E2%80%93Nichols_method
     // (accessed 2/4/2019)
 
-    // hyperparameters
+    // hyperparameters - stage0
     public final double test_offset = 60; // starting offset of robot (to test oscilations)
-    // stage 0
-    public final int failThreshold = 3; // how many amplitudes to consider for failed P-value
-    public final int succeedThreshold = 10; // how many amplitudes to consider for successful
-                                            // P-value
-    public final double failAvgRatio = .05; // fails if greater than x% gain/loss ratio
 
-    public int stage = 0; // 0 = tuning P to steady oscillations
+    // hyperparameters - stage1
+    public final int fail_threshold = 3; // how many amplitudes to consider for failed P-value
+    public final int succeed_threshold = 4; // how many successive fail-checks to pass until success
+                                            // a check is made each new amplitude, so 4-3
+    public final double fail_avg_ratio = .05; // fails if greater than x% gain/loss ratio
+
+    // robot control variables
     public double p = 0.02;
     public double i = 0;
     public double d = 0;
-
-    public double 
-
-    public double rotCounter = 0;
-    public double period;
-    public double error;
-    public double target = 0;
-    public Timer timer;
-
     public PIDController pidC;
     public double pidOut;
 
+    // control flow variables
+    public int stage = 0; // 0 = reset and init, 1 = testing p-value
+    public double pmax = p;
+    public double pmin = p;
+    public double failchecks = 0;
+
+    // Data Storage variables    
+    public Timer timer;
+    public double period;
+    public double error;
+    public double target = 0;
     public ArrayList<Double> datapoints = new ArrayList<Double>();
     public ArrayList<Double> amplitudes = new ArrayList<Double>();
     public double lastDatapoint;
@@ -70,17 +73,60 @@ public class AutoCalibrate extends Command implements PIDOutput {
     protected void execute() {
         double dp = getAngle(); // datapoint
         switch (stage) {
+            case -1:
+                // stop robot
+                timer.reset();
+                pidC = new PIDController(0, 0, 0, Robot.myNavX.ahrs, this);
+                stage = 0;
+                break;
             case 0:
+                // reset robot to test new P value
+                if(timer.hasPeriodPassed(1000))
+                break;
+            case 1:
                 // monitor amplitudes
                 if (isExtreme(lastDatapoint, dp)) {
                     addAmplitude(lastPeak, lastDatapoint);
-                }
-                if (amplitudes.size() > failThreshold) {    
-                    if(isFailedP(amplitudes, failThreshold, failAvgRatio)){
+
+                    // use binary search to iterate towards optimal-P
+                    if (amplitudes.size() > fail_threshold) {
+                        int failstate = isFailedP(amplitudes, fail_threshold, fail_avg_ratio);
+                        switch (failstate) {
+                            case 1:
+                                pmax = p;
+                                if (p == pmin) {
+                                    pmin /= 2;
+                                    p = pmin;
+                                    stage = -1; // set and try new p-value
+                                } else {
+                                    p = (pmin + pmax) / 2;
+                                    stage = -1; // reset and try new p-value
+
+                                }
+                                break;
+                            case -1:
+                                pmin = p;
+                                if (p == pmax) {
+                                    pmax *= 2;
+                                    p = pmax;
+                                    stage = -1; // reset and try new p-value
+                                } else {
+                                    p = (pmin + pmax) / 2;
+                                    stage = -1; // reset and try new p-value
+                                }
+                                break;
+                            case 0:
+                                failchecks++;
+                                if (failchecks == succeed_threshold) {
+                                    stage = 2;
+                                    System.out.println("Ku (ultimate gain) = " + p);
+                                }
+                        }
 
                     }
                 }
                 break;
+
         }
         addDataPoint(dp);
     }
@@ -90,7 +136,7 @@ public class AutoCalibrate extends Command implements PIDOutput {
     }
 
     public double getAngle() {
-        return (Robot.myNavX.ahrs.getYaw() + angleOffset);
+        return (Robot.myNavX.ahrs.getYaw() + test_offset);
     }
 
     // public double getAngleUnlooped() {
@@ -145,15 +191,18 @@ public class AutoCalibrate extends Command implements PIDOutput {
         return 0.0;
     }
 
-    public int isFailedP(ArrayList<Double> amplitudes, int failThreshold, double failAvgRatio) {
+    public int isFailedP(ArrayList<Double> amplitudes, int fail_threshold, double fail_avg_ratio) {
+        // 1 = amplitudes growing, -1 = shrinking amplitudes, 0 = stable amplitudes?
         int size = amplitudes.size();
         double sum = 0;
-        for (int i = 0; i < (size - failThreshold) - 1; i++) {
+        for (int i = 0; i < (size - fail_threshold) - 1; i++) {
             sum += amplitudes.get(i) / amplitudes.get(i + 1);
         }
         sum /= (size - 1);
-        if (sum > 1 + failAvgRatio ) {
+        if (sum > 1 + fail_avg_ratio) {
             return 1;
+        } else if (sum < 1 - fail_avg_ratio) {
+            return -1;
         }
         return 0;
     }
