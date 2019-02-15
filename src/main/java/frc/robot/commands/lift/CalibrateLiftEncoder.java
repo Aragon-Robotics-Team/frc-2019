190b6cdf8692018518b39b929e576dc2f7201d11
Daniel Z <5725958+dzil123@users.noreply.github.com>
2019-02-14 23:58:52 -0800

    Generalize MoveUntilLimitSwitch into MoveUntilResult

--- src/main/java/frc/robot/commands/lift/CalibrateLiftEncoder.java
+++ src/main/java/frc/robot/commands/lift/CalibrateLiftEncoder.java
@@ -1,20 +1,20 @@
 package frc.robot.commands.lift;
 
-import java.util.function.BooleanSupplier;
 import java.util.function.DoubleConsumer;
+import java.util.function.Supplier;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
-import frc.robot.commands.autonomous.MoveUntilLimitSwitch;
+import frc.robot.commands.MoveUntilResult;
 
 public class CalibrateLiftEncoder extends CommandGroup {
     static final double speed = 0.5;
     static final DoubleConsumer setSpeed = Robot.myLift.controller::setPercent;
-    static final BooleanSupplier getReverseLimit = Robot.myLift.controller::getReverseLimitSwitch;
+    static final Supplier<Boolean> getReverseLimit = Robot.myLift.controller::getReverseLimitSwitch;
 
     public CalibrateLiftEncoder() {
         // Go reverse until hits limit switch, then go forward until release
-        addSequential(new MoveUntilLimitSwitch(-speed, setSpeed, getReverseLimit, true));
-        addSequential(new MoveUntilLimitSwitch(speed, setSpeed, getReverseLimit, false));
+        addSequential(new MoveUntilResult<Boolean>(-speed, setSpeed, getReverseLimit, true));
+        addSequential(new MoveUntilResult<Boolean>(speed, setSpeed, getReverseLimit, false));
         // Talon SRX should reset encoder automatically when limit switch depressed
     }
 }
