2cec482b5d53136c137bc7956ee8c89de0ce6a16
dzil123 <5725958+dzil123@users.noreply.github.com>
2019-03-09 21:41:25 -0800

    Use class in vision from RIO

--- src/main/java/frc/robot/util/fieldmap/MapInference.java
+++ src/main/java/frc/robot/util/fieldmap/MapInference.java
@@ -6,11 +6,12 @@ import java.util.Comparator;
 import java.util.List;
 import org.opencv.core.Point;
 import frc.robot.util.fieldmap.Map.Target;
+import frc.robot.vision.CoordTransform;
 
 public class MapInference {
     public static Target[] getClosestTargets(Point robot_location, double target_direction) {
-        // target direction is in degrees, (-180,180), counterclockwise is positive, zero is
-        // horizontal
+        // target direction is in degrees, (-180,180), counterclockwise is positive,
+        // zero is horizontal
         List<Target> targets = new ArrayList<Target>(Arrays.asList(Map.targets));
         for (int i = 0; i < targets.size(); i++) {
             double angle_diff = targets.get(i).angle - target_direction;
@@ -23,9 +24,8 @@ public class MapInference {
 
         Target[] targets_array = new Target[targets.size()];
         targets_array = targets.toArray(targets_array);
-        Arrays.sort(targets_array, Comparator.comparingDouble(
-                (Target t) -> Math.abs(target_direction - CoordTransform.toPolar(new double[] {
-                        t.center.x - robot_location.x, t.center.y - robot_location.y})[1])));
+        Arrays.sort(targets_array, Comparator.comparingDouble((Target t) -> Math.abs(target_direction - CoordTransform
+                .toPolar(new double[] { t.center.x - robot_location.x, t.center.y - robot_location.y })[1])));
         return targets_array;
     }
 }
