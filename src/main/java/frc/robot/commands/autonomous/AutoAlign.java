f911eb311d1bccb90a1cc68891ec76ca2e986d97
dzil123 <5725958+dzil123@users.noreply.github.com>
2019-04-29 18:16:21 -0700

    changes

--- src/main/java/frc/robot/commands/autonomous/AutoAlign.java
+++ src/main/java/frc/robot/commands/autonomous/AutoAlign.java
@@ -4,12 +4,13 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class AutoAlign extends Command {
-    public int state; // 0-stopped, 1-turning
-    public double lastAngle;
+    // public int state; // 0-stopped, 1-turning
+    // public double lastAngle;
 
     public AutoAlign() {
         requires(Robot.myDrivetrain);
         requires(Robot.myAngle);
+        setTimeout(5);
     }
 
     protected void initialize() {
@@ -19,24 +20,24 @@ public class AutoAlign extends Command {
     protected void execute() {
         if (Robot.myVision.seeTarget) {
             double yaw = Robot.myVision.calculatedYaw;
-            if (yaw != lastAngle) {
-                lastAngle = yaw;
-                Robot.myAngle.setAngleOffset(yaw);
-            }
-            if (state == 0) {
-                state = 1;
-                Robot.myAngle.setEnabled(true);
-            }
+            // if (yaw != lastAngle) {
+            // lastAngle = yaw;
+            Robot.myAngle.setAngleOffset(yaw);
+            // }
+            // if (state == 0) {
+            // state = 1;
+            Robot.myAngle.setEnabled(true);
+            // }
         } else {
-            if (state == 1) {
-                state = 0;
-                Robot.myAngle.setEnabled(false);
-            }
+            // if (state == 1) {
+            // state = 0;
+            Robot.myAngle.setEnabled(false);
+            // }
         }
     }
 
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     protected void end() {
