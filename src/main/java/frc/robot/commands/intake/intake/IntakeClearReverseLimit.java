6e16371c1c28786c2ee7754bb19a43f770e66d6a
Daniel Z <5725958+dzil123@users.noreply.github.com>
2019-05-20 21:14:58 -0700

    YES! Finally fixed the issue with the lift pneumatics avoidance!

--- src/main/java/frc/robot/commands/intake/intake/IntakeClearReverseLimit.java
+++ src/main/java/frc/robot/commands/intake/intake/IntakeClearReverseLimit.java
@@ -6,5 +6,6 @@ import frc.robot.commands.MoveUntilResult;
 public class IntakeClearReverseLimit extends MoveUntilResult<Boolean> {
     public IntakeClearReverseLimit() {
         super(0.05, Robot.myIntake::set, Robot.myIntake.controller::getReverseLimitSwitch, false);
+        setInterruptible(false);
     }
 }
